664b6fac28d1869cb39d5c2b95fee18f4a651e6b
Ćompu Grande -- Gabriel <gabo2023brazil@gmail.com>
2021-07-09 08:09:36 -0500

    Adding stuf

--- villa-gabs/villa.js
+++ villa-gabs/villa.js
@@ -1,3 +1,23 @@
+//Variables estáticas globales
+//Imagenes
+const vaca = './vaca.png';
+const cerdo = './cerdo.png';
+const gallina = '.gallina.png';
+
+//Canvas
+const canvas = document.getElementById('villa-canvas');
+const papel = canvas.getContext('2d');
+
+const imagen  = new Image();
+imagen.src = tile;
+
+const dibujar = () =>{
+    papel.drawImage(tile, 0, 0);
+}
+
+imagen.addEventListener('load', dibujar);
+
+
 //Ecuación de aleatoriedad 
 const aleatoriedad = (maximo, minimo) => {
     const resultado = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
