3e2b6a531cec653173822b4d086aa6a1714cbc47
Gabriel Andres Fajardo Ortiz <gabo2023brazil@gmail.com>
2021-07-12 12:26:44 -0300

    Trying to solve problems

--- villa-gabs/villa.js
+++ villa-gabs/villa.js
@@ -1,28 +1,29 @@
 //Variables estáticas globales
 //Imagenes
-const vaca = './vaca.png';
-const cerdo = './cerdo.png';
-const gallina = '.gallina.png';
+const vaca = document.getElementById('vaquita');
+const cerdo = document.getElementById('cerdito');
+const gallina = document.getElementById('pollo');
+const tile = document.getElementById('fondo');
 
 //Canvas
 const canvas = document.getElementById('villa-canvas');
 const papel = canvas.getContext('2d');
 
-const imagen  = new Image();
-imagen.src = tile;
+let img = new Image();
+img.src = './tile.png';
 
-const dibujar = () =>{
+img.addEventListener('load', dibujar)
+
+function dibujar (){
     papel.drawImage(tile, 0, 0);
-}
-
-imagen.addEventListener('load', dibujar);
-
-
+}    
+    
 //Ecuación de aleatoriedad 
+/*
 const aleatoriedad = (maximo, minimo) => {
     const resultado = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
     return resultado;
-}
+}*/
 
 let n;
 
